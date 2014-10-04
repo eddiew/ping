77f674fa06995298eef25b1c570375fe502400d4
Jack Boffa <jaffob77@gmail.com>
2014-10-04 11:57:03 -0700

    add canvas to JS and fill with red

--- ping.js
+++ ping.js
@@ -1,6 +1,13 @@
 // Box2d & canvas initialization
 
+var CANVAS_WIDTH = 640;
+var CANVAS_HEIGHT = 480;
 
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+
+context.fillStyle = "#ff0000";
+context.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
 // Game logic
 
