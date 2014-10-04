f3553fa370eed75216c46316090d676128707ca6
eddiew <eddiew@stanford.edu>
2014-10-04 12:04:52 -0700

    fix merge conflict in ping

--- ping.js
+++ ping.js
@@ -1,9 +1,5 @@
 // Canvas initialization
 
-
-
-// Box2d initialization
-
 var CANVAS_WIDTH = 640;
 var CANVAS_HEIGHT = 480;
 
@@ -12,6 +8,15 @@ var context = canvas.getContext("2d");
 
 context.fillStyle = "#ff0000";
 context.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+// Box2d initialization
+
+window.onload = function() {
+	using(Box2D, "b2.+");
+	init();
+	changeTest();
+	animate();
+};
 
 // Game logic
 
