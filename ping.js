308e945062185735e10e9d31a07ad89957698acc
eddiew <eddiew@stanford.edu>
2014-10-04 12:40:43 -0700

    dynamic canvas size

--- ping.js
+++ ping.js
@@ -1,13 +1,25 @@
 // Canvas initialization
 
-var CANVAS_WIDTH = 640;
-var CANVAS_HEIGHT = 480;
+var W, H;
 
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 
+var maxW = window.innerWidth, maxH = window.innerHeight;
+if (maxW / maxH > 16 / 9) {
+	context.canvas.width = maxH * 16 / 9;
+	context.canvas.height = maxH;
+}
+else {
+	context.canvas.width = maxW;
+	context.canvas.height = maxW * 9 / 16;
+}
+W = context.canvas.width;
+H = context.canvas.height;
+canvas.style.width = W;
+canvas.style.height = H;
 context.fillStyle = "#ff0000";
-context.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+context.fillRect(0, 0, W, H);
 
 // Box2d initialization
 
